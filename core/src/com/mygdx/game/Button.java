7510f20d92612a9a24d0ab55b734bdce57e2c840
tonplamm <tonplamm@10.2.51.17>
2016-11-15 15:04:24 +0700

    Button Class is cleaner!

--- core/src/com/mygdx/game/Button.java
+++ core/src/com/mygdx/game/Button.java
@@ -13,8 +13,8 @@ public class Button extends ScreenAdapter {
 	private Texture pressedColor_button;
 	
 	public Button(BeatGame beatGame) {
-		this.beatGame = beatGame;
 		batch = beatGame.batch;
+		this.beatGame = beatGame;
 		pressedColor_button = new Texture("buttonWhenPressed.png");
 	}
 	
